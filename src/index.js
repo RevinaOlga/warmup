817378c501c4918aa6a13984242599f071048f38
Olga <revina.volga@gmail.ru>
2019-02-21 17:17:23 +0300

    add task warmup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+
+  return temperature * (9 / 5) + 32;
+  // if (temperature === -20) {
+  //   return -4;
+  // }
+  // if (temperature === 0) {
+  //   return 32;
+  // }
+  // if (temperature === 15) {
+  //   return 59;
+  // }
+  // if (temperature === 40) {
+  //   return 104;
+  // }
 };
